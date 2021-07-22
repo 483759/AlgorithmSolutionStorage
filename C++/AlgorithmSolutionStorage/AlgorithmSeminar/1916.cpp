df88026d7a5c14691994e47fef573acc714ca6eb
윤이진 <483759@naver.com>
2021-07-23 00:43:00 +0900

    2021/07/22

--- C++/AlgorithmSolutionStorage/AlgorithmSeminar/1916.cpp
+++ C++/AlgorithmSolutionStorage/AlgorithmSeminar/1916.cpp
@@ -1,56 +1,42 @@
 #include <cstdio>
-#define t 1000000007
-#define ull unsigned long long
-ull n;
-ull fibo(ull k) {
-	if (!k)return 0;
-	if (k == 1)return 1;
-	return (fibo(k-1)%t)+
+#include <vector>
+#include <queue>
+#define pii pair<int,int>
+#define INF 987654321
+using namespace std;
+
+vector<vector<pii>> g;
+int n, m,s,f;
+
+int dijkstra() {
+	vector<int> dist(n+1,INF);
+	priority_queue<pii> pq;
+
+	pq.push(make_pair(0, s));
+	dist[s] = 0;
+
+	while (!pq.empty()) {
+		auto cur = pq.top(); pq.pop();
+		if (cur.first > dist[cur.second])continue;
+
+		for (auto x : g[cur.second]) 
+		if(dist[x.second]>x.first+dist[cur.second]){
+			dist[x.second] = x.first + dist[cur.second];
+			pq.push(make_pair(dist[x.second], x.second));
+		}
+	}
+	return dist[f];
 }
+
 int main() {
-	scanf("%lld", &n);
+	scanf("%d%d", &n, &m);
+	g = vector<vector<pii>>(n + 1);
+
+	for (int i = 0,u,v,w; i < m; i++) {
+		scanf("%d%d%d", &u, &v, &w);
+		g[u].push_back(make_pair(w, v));
+	}
+	scanf("%d%d", &s, &f);
+	printf("%d", dijkstra());
 	return 0;
 }
-//#include <cstdio>
-//#include <queue>
-//#include <vector>
-//#include <algorithm>
-//#define INF 987654321
-//using namespace std;
-//int n, m;
-//vector<pair<int, int>> edge[1001];
-//int dijkstra(int u, int v) {
-//	int dist[1005];
-//	priority_queue<pair<int, int>> pq;
-//	fill(&dist[0], &dist[n + 1], INF);
-//	pq.push({ 0,u });
-//	dist[u] = 0;
-//
-//	while (!pq.empty()) {
-//		int cost = -pq.top().first;
-//		int cur = pq.top().second;
-//		pq.pop();
-//		
-//		for (int i = 0; i < edge[cur].size(); i++) {
-//			int next = edge[cur][i].first;
-//			int ncost = edge[cur][i].second;
-//
-//			if (dist[cur] + ncost < dist[next]) {
-//				dist[next] = dist[cur] + ncost;
-//				pq.push({ -dist[next],next });
-//			}
-//		}
-//	}
-//	return dist[v];
-//}
-//int main() {
-//	scanf("%d%d", &n, &m);
-//	for (int i = 0; i < m; i++) {
-//		int u, v, w;
-//		scanf("%d%d%d", &u, &v, &w);
-//		edge[u].push_back(make_pair(v, w));
-//	}int u, v;
-//	scanf("%d%d", &u, &v);
-//	printf("%d", dijkstra(u,v));
-//	return 0;
-//}
