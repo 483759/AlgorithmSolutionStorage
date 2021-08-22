ec49149e2d6de5b1c3c6db9f5e54b1a1fe220561
윤이진 <483759@naver.com>
2021-08-22 22:57:04 +0900

    BOJ2240

--- C++/AlgorithmSolutionStorage/AlgorithmSeminar/18112.cpp
+++ C++/AlgorithmSolutionStorage/AlgorithmSeminar/18112.cpp
@@ -4,39 +4,33 @@
 #include <math.h>
 #include <map>
 #include <queue>
+#define ull unsigned long long
 using namespace std;
 
-string s, f;
-map<string, int> m;
+ull s, f;
 bool visit[2500];
 
-bool safe(int num) {
+bool safe(ull num) {
 	return num >= 0 && log2(num) < 11 && !visit[num];
 }
 
-int binaryToDecimal(string bs) {
-	int num = 0;
-	for (int i = 0; i < bs.length(); i++) {
-		if (bs[i] == '1') num += pow(2, bs.length() - 1 - i);
+ull binaryToDecimal(ull bs) {
+	ull num = 0, bin = 1;
+
+	while (bs) {
+		if (bs % 2) num += bin;
+		bin *= 2; bs /= 10;
 	}
+
 	return num;
 }
 
-string decimalToBinary(int num) {
-	string bs;
-
-	while (num > 0) {
-		bs.push_back((num % 2) + '0');
-		num /= 2;
-	}
-	reverse(bs.begin(), bs.end());
-	return bs;
-}
-
 int main() {
-	cin >> s >> f;
+	scanf("%lld%lld", &s, &f);
+	s = binaryToDecimal(s);
+	f = binaryToDecimal(f);
 
-	queue<string> q;
+	queue<ull> q;
 	q.push(s);
 
 	for (int time = 0;; time++) {
@@ -44,30 +38,29 @@ int main() {
 		if (!size)break;
 
 		while (size--) {
-			string cur = q.front(); q.pop();
+			ull cur = q.front(); q.pop();
 			if (cur == f) {
-				cout << time;
+				printf("%d", time);
 				return 0;
 			}
 
-			int num = binaryToDecimal(cur);
-			for (int i = 1; i < cur.length(); i++) {
-				cur[i] = (!(cur[i] - '0')) + '0';
-				if (safe(binaryToDecimal(cur))) {
-					q.push(cur); visit[binaryToDecimal(cur)] = true;
+			int index = ceil(log2(cur));
+			for (int i = 1; i < index; i++) {
+				cur = cur ^ (1L << (ull)(index - 1 - i));
+				if (safe(cur)) {
+					q.push(cur); visit[cur] = true;
 				}
-				cur[i] = (!(cur[i] - '0')) + '0';
-			}
-			if (safe(num + 1)) {
-				q.push(decimalToBinary(num + 1)); visit[num + 1] = true;
-			}
-			if (safe(num - 1)) {
-				q.push(decimalToBinary(num - 1)); visit[num - 1] = true;
+				cur = cur ^ (1L << (ull)(index - 1 - i));
 			}
 
+			if (safe(cur + 1)) {
+				q.push(cur + 1); visit[cur + 1] = true;
+			}
+			if (safe(cur - 1)) {
+				q.push(cur - 1); visit[cur - 1] = true;
+			}
 		}
 	}
-
 
 	return 0;
 }
